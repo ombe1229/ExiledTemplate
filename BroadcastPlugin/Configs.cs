7b7cb1eb797082c9a7f4bc16e9f752a68a0a806b
ombe1229 <h3236516@gmail.com>
2021-01-27 13:34:15 +0900

    Feat: add BroadcastPlugin

---
+++ BroadcastPlugin/Configs.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using Exiled.API.Interfaces;
+
+namespace BroadcastPlugin
+{
+    public class Configs : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+
+        [Description("Should welcome message be shown?")]
+        public bool WelcomeMessageEnable { get; private set; } = true;
+
+        [Description("Welcome message")]
+        public string WelcomeMessage { get; private set; } = "Welcome, %PLAYER%!";
+    }
+}
---
+++ BroadcastPlugin/EventHandlers.cs
@@ -0,0 +1,37 @@
+﻿using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+using Respawning;
+
+namespace BroadcastPlugin
+{
+    public class EventHandlers
+    {
+        private readonly broadcastPlugin _pluginInstance;
+        public EventHandlers(broadcastPlugin pluginInstance) => this._pluginInstance = pluginInstance;
+
+        internal void OnRoundStarting()
+        {
+            if (!_pluginInstance.Config.IsEnabled) return;
+            broadcastPlugin.IsStarted = true;
+        }
+
+        internal void OnRoundEnded(RoundEndedEventArgs ev)
+        {
+            broadcastPlugin.IsStarted = false;
+        }
+
+        internal void OnRoundRestarting()
+        {
+            broadcastPlugin.IsStarted = false;
+        }
+
+        internal void OnPlayerJoined(JoinedEventArgs ev)
+        {
+            if (!_pluginInstance.Config.WelcomeMessageEnable) return;
+            Player evPlayer = ev.Player;
+            string welcomeMessage =
+                _pluginInstance.Config.WelcomeMessage.Replace("%PLAYER%", evPlayer.DisplayNickname);
+            evPlayer.Broadcast(10, welcomeMessage);
+        }
+    }
+}
